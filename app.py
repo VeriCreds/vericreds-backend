7e8d5d291e21bf7c74c7fc3d04843c62803aa226
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2023-07-26 11:30:53 -0400

    work on backend auth

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime, timedelta, timezone
 from util import mongo
 from moralis import auth
 from flask_cors import CORS
@@ -7,10 +8,12 @@ from dotenv import dotenv_values
 
 app = Flask(__name__)
 CORS(app)
+# cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 config = dotenv_values("./.env")
 api_key = config["MORALIS_API_KEY"]
 
+print("api_key", api_key)
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -22,17 +25,31 @@ def hello_world():  # put application's code here
 @app.route('/requestChallenge', methods=["GET"])
 def reqChallenge():
     args = request.args
+
+    # get current UTC time
+    now = datetime.now(timezone.utc)
+
+    # create timestamps for expiration and not_before
+    # for instance, the message will be valid for the next 5 minutes
+    expiration_time = now + timedelta(minutes=5)
+    not_before_time = now
+
+    # format as ISO 8601
+    expiration_time_str = expiration_time.isoformat().replace("+00:00", "Z")
+    not_before_time_str = not_before_time.isoformat().replace("+00:00", "Z")
+
     body = {
         "domain": "localhost:3000",
         "chainId": args.get("chainId"),
         "address": args.get("address"),
         "statement": "Please confirm login",
         "uri": "http://localhost:3000/",
-        "expirationTime": "2023-01-01T00:00:00.000Z",
-        "notBefore": "2023-01-01T00:00:00.000Z",
+        "expirationTime": expiration_time_str,
+        "notBefore": not_before_time_str,
         "resources": ['https://docs.moralis.io/'],
         "timeout": 30,
     }
+
     result = auth.challenge.request_challenge_evm(
         api_key=api_key,
         body=body,
@@ -47,6 +64,11 @@ def verifyChallenge():
         "message": args.get("message"),
         "signature": args.get("signature"),
     }
+
+    # Log the message and the current server time
+    print("Message: ", args.get("message"))
+    print("Current Server Time: ", datetime.now().isoformat())
+
     result = auth.challenge.verify_challenge_evm(
         api_key=api_key,
         body=body
